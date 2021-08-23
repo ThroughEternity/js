2f893a280d7cbcdcde38aa8aaa1cb10877528ac6
Dima <eternity1666@gmail.com>
2021-08-23 20:57:38 +0400

    add task5

--- task4/script.js
+++ task4/script.js
@@ -5,7 +5,6 @@ let linkProm = document.querySelector(`#prompt`)
 linkProm.addEventListener(`click`, () => {
 let tex = prompt('Измените текст ссылки')  
     
-
 linkProm.innerHTML = tex
    
   })   
--- task5/script.js
+++ task5/script.js
@@ -1,8 +1,24 @@
-let tex = document.getElementById('text')
+let tex = document.querySelector('input[type="text"]')
 
 let result = document.querySelector('#duplicateField')
 
+let but = document.querySelector('button[type="submit"]')
 
+
+
+but.onclick = function(e) {
+    console.log(tex.value )
+    tex.value = "";
+    result.innerHTML = ""
+  }
+  
+
+  
 tex.oninput = function() {
+   
     result.innerHTML = tex.value  
+
 }
+
+
+
